5c97db96647533f4965d5ffcc66b633efdc24da3
Thomas Boutell <tom@apostrophecms.com>
2023-11-13 12:24:05 -0500

    cute little house that should render as a 37 point emoji if options are properly available to widgets, both when adding/editing them and at full page render time

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,9 +1,11 @@
 import RichTextWidget from './RichTextWidget.astro';
 import ImageWidget from './ImageWidget.astro';
+import HouseWidget from './HouseWidget.astro';
 
 const widgetTypes = {
   '@apostrophecms/rich-text': RichTextWidget,
-  '@apostrophecms/image': ImageWidget
+  '@apostrophecms/image': ImageWidget,
+  'house': HouseWidget
 };
 
 export default widgetTypes;
