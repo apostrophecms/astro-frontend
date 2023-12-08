e4f3a88a65ad345c70e3abbfeb73226d4e14901f
Thomas Boutell <tom@apostrophecms.com>
2023-12-08 14:39:57 -0500

    two column widget to demonstrate this better, use our module name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from 'astro/config'
-import apostrophe from '@michelin-cxf/astro-apostrophe-integration'
+import { defineConfig } from 'astro/config';
+import apostrophe from '@apostrophecms/astro-integration';
 
 export default defineConfig({
   output: 'server',
   integrations: [
     apostrophe({
-        aposHost: 'http://localhost:3000',
-        widgetsMapping: './src/widgets',
-        templatesMapping: './src/templates',
-        forwardHeaders: [
-            'content-security-policy', 
-            'strict-transport-security', 
-            'x-frame-options',
-            'referrer-policy',
-            'cache-control',
-            'host'
-        ]
+      aposHost: 'http://localhost:3000',
+      widgetsMapping: './src/widgets',
+      templatesMapping: './src/templates',
+      forwardHeaders: [
+        'content-security-policy', 
+        'strict-transport-security', 
+        'x-frame-options',
+        'referrer-policy',
+        'cache-control',
+        'host'
+      ]
     })
   ]
 });
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,11 +1,13 @@
 import RichTextWidget from './RichTextWidget.astro';
 import ImageWidget from './ImageWidget.astro';
 import HouseWidget from './HouseWidget.astro';
+import TwoColumnWidget from './TwoColumnWidget.astro';
 
 const widgetTypes = {
   '@apostrophecms/rich-text': RichTextWidget,
   '@apostrophecms/image': ImageWidget,
-  'house': HouseWidget
+  'house': HouseWidget,
+  'two-column': TwoColumnWidget
 };
 
 export default widgetTypes;
